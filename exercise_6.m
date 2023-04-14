ada39fb0e73a4d975f97294aea198497a20c7472
Aziz251984 <67162178+Aziz251984@users.noreply.github.com>
2023-04-14 16:09:25 +0900

    Add files via upload

---
+++ exercise_6.m
@@ -0,0 +1,5 @@
+% Prompt the user for a length and units
+length = input('Enter the length: ');
+units = input('Enter the units: ', 's');
+% Print the length and units
+fprintf('Length: %d %s\n', length, units);
