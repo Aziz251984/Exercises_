238ae2799946a3f8b50b141473047e0db527e2b9
Azizjon <67162178+Aziz251984@users.noreply.github.com>
2023-04-14 17:30:26 +0900

    Add files via upload

---
+++ exercise_4.m
@@ -0,0 +1,18 @@
+%Ex 1
+%This is basic practise_1
+radius=4;
+area=pi*radius^2;
+disp('area=');disp(area);
+
+
+circum=2*pi*radius;
+disp('circum=');disp(circum);
+%%%%%
+%Ex 2
+%This is excersie to practise
+t = 0:0.02:0.6;
+sin=10;%Variables
+
+y=3*sin*(pi*10*t);
+plot(t,y);
+title('Practise equation');
